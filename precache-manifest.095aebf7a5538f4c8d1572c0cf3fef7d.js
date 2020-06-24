9589886f158284f82b7bcd720b43e5d17a973ce9
toprekeeb <ckb0v0@gmail.com>
2020-06-24 21:24:27 +0930

    Updates

---
+++ precache-manifest.095aebf7a5538f4c8d1572c0cf3fef7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b87405319db95fb15f4d3da029825a",
+    "url": "/hktaximeter/index.html"
+  },
+  {
+    "revision": "ebf89c18366befb42216",
+    "url": "/hktaximeter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/hktaximeter/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hktaximeter/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf89c18366befb42216",
+    "url": "/hktaximeter/static/js/main.17eedbe7.chunk.js"
+  },
+  {
+    "revision": "1fc2dafe2c52548d7ba4",
+    "url": "/hktaximeter/static/js/runtime-main.f61d0c90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hktaximeter/static/media/logo.5d5d9eef.svg"
+  }
+]);
